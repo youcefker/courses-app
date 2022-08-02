021d89748190fe8fff525723910a9a9c57a2dbfd
youcefker <youcefdev132@gmail.com>
2022-08-02 16:05:26 +0100

    deploying

--- api/Services/EmailServices.js
+++ api/Services/EmailServices.js
@@ -1,34 +1,32 @@
-const nodemailer = require('nodemailer')
-const mg = require("nodemailer-mailgun-transport")
+const nodemailer = require("nodemailer")
+const sgTransport = require('nodemailer-sendgrid-transport');
+
 module.exports = {
-    sendMail: async (to, from, body, callBack) => {
-        try {
-            const mailgunAuth = {
-                auth: {
-                  api_key: "cc452de491c929915a10994843cf9867-18e06deb-6e302648",
-                  domain: "sandboxf03f5c1e49764f9fbeba5ebcfed76422.mailgun.org"
-                }
-              }
-              
-              const smtpTransport = nodemailer.createTransport(mg(mailgunAuth))
-              
-              const mailOptions = {
-                from,
-                to,
-                subject: "Investinsmart email verification",
-                html: body
-              }
-              
-              smtpTransport.sendMail(mailOptions, function(error, response) {
-                if (error) {
-                    return callBack(true)
-                } else {
-                    return callBack(false, response)
-                }
-              })
-        } catch(err){
-            console.log(err)
-            return callBack(true)
-        }
+  sendMail : (to, from, body, callBack) => {
+    var options = {
+      auth: {
+        api_key: process.env.SENDGRID_API_KEY
+      }
     }
-} 
+    
+    var client = nodemailer.createTransport(sgTransport(options));
+    
+    var email = {
+      from,
+      to,
+      subject: 'Confirm your account',
+      text: 'Hello,',
+      html: body
+    };
+    
+    client.sendMail(email, function(err, info){
+        if (err ){
+          console.log(err)
+          return callBack(true)
+        }
+        else {
+          return callBack(false, info)
+        }
+    });
+  }
+}
